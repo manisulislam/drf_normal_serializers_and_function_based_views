e3351975199a2dc280ff4ba41c53839aff46a6f9
anisul islam <anisulislam980340@gmail.com>
2024-02-14 15:09:00 +0600

    solved json parse and restframwork response problem

--- book_app/api/views.py
+++ book_app/api/views.py
@@ -1,40 +1,40 @@
 from rest_framework.decorators import api_view
-from django.http import JsonResponse,HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 from .serializers import BookSerializer
 from book_app.models import Book
 from rest_framework.parsers import JSONParser
-
+from rest_framework.response import Response
 
 @api_view(['GET', 'POST'])
 def book_list(request):
     if request.method == 'GET':
         all_book= Book.objects.all()
         serializer= BookSerializer(all_book,many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     if request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = BookSerializer(data=data)
+        
+        serializer = BookSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
-@api_view(['GET', 'POST','DELETE'])
+@api_view(['GET', 'PUT','DELETE'])
 def book_detail(request,pk):
     if request.method == 'GET':
         book = Book.objects.get(pk=pk)
         serializer = BookSerializer(book)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     if request.method == 'PUT':
-        data = JSONParser().parse(request)
+        
         book = Book.objects.get(pk=pk)
-        serializer = BookSerializer(book, data=data)
+        serializer = BookSerializer(book, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
 
     if request.method == 'DELETE':
         book = Book.objects.get(pk=pk)
